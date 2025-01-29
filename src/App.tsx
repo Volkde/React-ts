e86a68f522162d25b07d368877bb018c44d65859
volkde <ilyaprofit@gmail.com>
2025-01-29 22:07:49 +0100

    "and Lesson10"

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import GlobalStyles from './styles/GlobalStyles'
 // import Lesson06 from './lessons/Lesson06/Lesson06'
 // import Lesson07 from './lessons/Lesson07/Lesson07'
 // import Lesson08 from './lessons/Lesson08/Lesson08'
-import Lesson09 from './lessons/Lesson09/Lesson09'
+import Lesson10 from './lessons/Lesson10/Lesson10'
 
 //homeworks
 // import Homework08 from './homeworks/Homework08/Homework08'
@@ -27,7 +27,7 @@ function App() {
       {/* <Lesson08 /> */}
       {/* <Homework08 /> */}
       {/* Topic: Control components, useEffect */}
-      <Lesson09 />
+      <Lesson10 />
     </>
   )
 }
