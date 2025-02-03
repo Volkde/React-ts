d95ad325b480593daa8ce9d47baa42985ba0efe2
volkde <ilyaprofit@gmail.com>
2025-02-03 17:22:26 +0100

    Homework13

---
+++ src/components/EmployeeForm/styles.ts
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+export const EmployeeFormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 40px;
+  width: 400px;
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 8px;
+`;
+
+export const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const CheckboxContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
